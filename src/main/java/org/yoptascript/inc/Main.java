f37a6dd80e023e715051670c63e14e3a80ca3db3
Zhalkhas <jalgas1998@gmail.com>
2019-10-12 01:54:15 +0600

    Base project

--- src/main/java/org/yoptascript/inc/Main.java
+++
@@ -1,7 +0,0 @@
-package org.yoptascript.inc;
-
-public class Main {
-    public static void main(String[] args){
-
-    }
-}
---
+++ src/main/java/org/yoptascript/inc/MyServlet.java
@@ -0,0 +1,27 @@
+package org.yoptascript.inc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/"})
+public class MyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public MyServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
