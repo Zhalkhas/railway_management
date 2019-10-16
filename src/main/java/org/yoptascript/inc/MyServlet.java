4816cc130b70f2a2eb9df7287e329f2c0f7d5321
Aida <aida.eduard@nu.edu.kz>
2019-10-16 17:11:02 +0600

    checking git

--- src/main/java/org/yoptascript/inc/MyServlet.java
+++ src/main/java/org/yoptascript/inc/MyServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet(urlPatterns = {"/servlet"})
 public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
